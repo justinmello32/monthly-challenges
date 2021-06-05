0322c39b1a16726df9a71913d58fa1a72d5acd07
Justin Mello <mello23@llnl.gov>
2021-06-05 12:50:57 -0700

    Adding better workflow for challenge_text var.

--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    "january": "This is something here",
+    "february": "This is something here",
+    "march": "Something here",
+    "april": "Something here",
+    "may": "Something here",
+    "june": "Something here",
+    "july": "Something here",
+    "august": "Something here",
+    "september": "Something here",
+    "october": "Something here",
+    "november": "Something here",
+    "december": "Something here",
+
+}
+
 # Create your views here.
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat not meat"
-    elif month == "fabruary":
-        challenge_text = "Something else"
-    else:
-        return HttpResponseNotFound("This month is not supported")
-
-    return HttpResponse("Something")
+    
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponse("This month is not supported")
